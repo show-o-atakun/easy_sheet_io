8b4e8c2b4268fff6868c60ad8e14d8087a8ce9fe
showata <shun_yamaguchi_tc@live.jp>
2023-02-24 21:52:44 +0900

    avoid_nilerror

--- lib/to_csv.rb
+++ lib/to_csv.rb
@@ -28,7 +28,9 @@ class Daru::DataFrame
 	# ver.0.3.8~ Convert Daru::DF encoding
 	def convert_enc!(from: "cp932", to: "utf-8")
 		self.vectors.each do |col|
-			self[col] = self[col].each {|val| val.encode!(to, from_encoding: from) } if self[col][0].is_a?(String)
+			if self[col][0].is_a?(String)
+				self[col] = self[col].each {|val| val.encode!(to, from_encoding: from) if !val.nil? }
+			end
 		end
 	end
 
